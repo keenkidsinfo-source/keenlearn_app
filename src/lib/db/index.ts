bd957baa87226d06ad8de1999e9a85d6cb81c553
Anjana Venkat <anjana.venkat@gmail.com>
2026-09-05 20:05:44 -0700

    fix: science week link, G1-2/G3-4 W2 coding, connection pool, storage routes

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -3,5 +3,5 @@ import postgres from 'postgres'
 import * as schema from './schema'
 
 const connectionString = process.env.DATABASE_URL!
-const client = postgres(connectionString, { prepare: false, max: 1 })
+const client = postgres(connectionString, { prepare: false, max: 3 })
 export const db = drizzle(client, { schema })
